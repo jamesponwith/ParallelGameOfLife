c43e03ee9a272982d5ccafed9e84b0c4d4440209
Patrick Hall <patrickhall@sandiego.edu>
2017-11-11 11:58:13 -0800

    wrote command line args snippet

--- gol.c
+++ gol.c
@@ -15,12 +15,45 @@
 #include <sys/time.h>
 #include <string.h>
 
+// Forward function declarations
+
+
+
+/**
+ * Prints out a reminder of how to run the program.
+ *
+ * @param executable_name String containing the name of the executable
+ */
+void usage(char *executable_name) {
+	printf("Usage: %s *rest of the args*", executable_name);
+}
+
 
 int main(int argc, char *argv[]) {
 
 	// Step 1: Parse command line args (I recommend using getopt again).
 	// You need to support the "-c" and "-v" options for the basic requirements.
 	// The advanced requirements require you to add "-l" and "-n" options.
+	
+	opterr = 0;
+	int c = -1;
+
+	while ((c == getopt(argc, argv, "")) != -1) {
+		switch(c) {
+			case 'c':
+				break;
+			case 'v':
+				break;
+			case 'l':
+				break;
+			case 'n':
+				break;
+			default:
+				usage(argv[0]);
+				exit(1);
+		}
+
+	}
 	
 	// Step 2: Read in the configuration file and use it to initialize your game
 	// board. Write a function to do this for you.
